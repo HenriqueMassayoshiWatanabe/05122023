5e53bf37300ed071b896d0c36b12e4b408b41294
YoshiWatanabe <146768123+HenriqueMassayoshiWatanabe@users.noreply.github.com>
2023-12-07 16:56:01 -0300

    Add files via upload

---
+++ CreateTableAllTypesCRUD.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class CreateTableAllTypesCRUD {
+    public static void main(String[] args) {
+        String status = "Nada aconteceu";
+        try {
+            Connection conn = MySQLConnector.conectar();
+            String strSqlCreate = "CREATE TABLE `teste`.`tbl_tipos_dados` (`id` INT NOT NULL AUTO_INCREMENT , `varchar` VARCHAR(255) NULL , `text` TEXT NULL , `date` DATE NULL , `tinyint` TINYINT NULL , `smallint` SMALLINT NULL , `mediumint` MEDIUMINT NULL , `bigint` BIGINT NULL , `decimal` DECIMAL NULL , `float` FLOAT NULL , `double` DOUBLE NULL , `real` REAL NULL , `bit` BIT NULL , `boolean` BOOLEAN NULL , `serial` SERIAL NULL , `datetime` DATETIME NULL , `timestamp` TIMESTAMP NULL , `time` TIME NULL , `year` YEAR NULL , `char` CHAR NULL , `tinytext` TINYTEXT NULL , `mediumtext` MEDIUMTEXT NULL , `longtext` LONGTEXT NULL , `binary` BINARY BINARY NULL , `varbinary` VARBINARY BINARY NULL , `tinyblob` TINYBLOB NULL , `blob` BLOB NULL , `mediumblob` MEDIUMBLOB NULL , `longblob` LONGBLOB NULL , `enum` ENUM NULL , `set` SET NULL , `geometry` GEOMETRY NULL , `point` POINT NULL , `linestring` LINESTRING NULL , `polygon` POLYGON NULL , `multipoint` MULTIPOINT NULL , `multilinestring` MULTILINESTRING NULL , `multipolygon` MULTIPOLYGON NULL , `geometrycollection` GEOMETRYCOLLECTION NULL , `json` JSON NULL , PRIMARY KEY (`id`));";
+            Statement stmSql = null;
+            stmSql = conn.createStatement();
+            stmSql.addBatch(strSqlCreate);
+            stmSql.executeBatch();
+            status = "Ok! Tabela criada.";
+            stmSql.close();
+        } catch (SQLException e) {
+            status = "Erro SQLException: " + e;
+        } catch (Exception e) {
+            status = "Erro Exception: " + e;
+        }
+        System.out.println(status);
+    }
+}
